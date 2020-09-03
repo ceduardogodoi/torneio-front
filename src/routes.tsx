b7323b9569ce913c9faf94d1a4e6cc6c2f420ede
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-09-03 00:01:46 -0300

    Cria rotas que serão renderizados ao centro

---
+++ src/routes.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Toolbar, makeStyles, Theme, createStyles } from '@material-ui/core';
+
+import Associacao from './pages/Associacao';
+
+const useStyles = makeStyles<Theme>((theme: Theme) =>
+  createStyles({
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3)
+    }
+  })
+);
+
+const Router = () => {
+  const classes = useStyles();
+
+  return (
+    <main className={classes.content}>
+      <Toolbar />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/associacao" component={Associacao} />
+        </Switch>
+      </BrowserRouter>
+    </main>
+  );
+};
+
+export default Router;
