8caceb567b2fe49064666a8347212f4e5ae745c2
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-09-02 23:06:54 -0300

    Cria NavDrawer

---
+++ src/components/NavDrawer.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  Drawer,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  createStyles,
+  Theme
+} from '@material-ui/core';
+import DescriptionIcon from '@material-ui/icons/Description';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0
+    },
+    drawerPaper: {
+      width: drawerWidth
+    },
+    toolbar: theme.mixins.toolbar
+  })
+);
+
+const NavDrawer: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper
+      }}
+      anchor="left"
+    >
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        {['Associação'].map(text => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              <DescriptionIcon />
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Drawer>
+  );
+};
+
+export default NavDrawer;
